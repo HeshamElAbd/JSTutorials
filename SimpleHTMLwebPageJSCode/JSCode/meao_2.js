caa1b1b5751df1e7c6be58f1fd9c3fa45b4cb384
Hesham El Abd <30498915+HeshamElAbd@users.noreply.github.com>
2019-08-24 10:07:33 +0200

    Add files via upload

---
+++ SimpleHTMLwebPageJSCode/JSCode/meao_2.js
@@ -0,0 +1,10 @@
+/**
+ * @Author: Hesham El Abd
+ * @Description: A simple JS file that illustrate how to include a JS script in an 
+ * HTML web-page
+ * @Date: 24.08.2019
+ */
+function cuddle(){
+	alert("meoa meao")
+	alert("This means I need more food, Human")
+}
